3e616e93b5fb2327350c8477fc951e8531512114
pravi-97 <praveesh.894@gmail.com>
2023-08-27 20:06:01 +0530

    Basic Build

---
+++ script/script.js
@@ -0,0 +1,8 @@
+function doStuff() {
+    var col1 = document.getElementById("col-left");
+    var col2 = document.getElementById("col-right");
+    var mainImg = document.getElementById("main-img");
+    col1.style.transform = "translateX(0%)";
+    col2.style.transform = "translateX(0%)";
+    mainImg.style.width = "60%"
+}
