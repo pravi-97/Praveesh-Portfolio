f96b2abb91c737b78f0f88f3c9c1c20b36d40482
pravi-97 <praveesh.894@gmail.com>
2023-08-27 23:23:14 +0530

    minor changes

--- script/script.js
+++ script/script.js
@@ -1,8 +1,19 @@
 function doStuff() {
-    var col1 = document.getElementById("col-left");
-    var col2 = document.getElementById("col-right");
+    var colLeft = document.getElementById("col-left");
+    var colRight = document.getElementById("col-right");
     var mainImg = document.getElementById("main-img");
-    col1.style.transform = "translateX(0%)";
-    col2.style.transform = "translateX(0%)";
+    colLeft.style.transform = "translateX(0%)";
+    colRight.style.transform = "translateX(0%)";
     mainImg.style.width = "60%"
+    setTimeout(runSocial,2000)
+}
+
+function runSocial(){
+    var socialLinks = document.getElementById("social-links")
+    socialLinks.style.transform = "translate(-50%, -50%) translateX(0%)";
+
+    var social = document.getElementsByClassName("social");
+    for (var i = 0; i < social.length; i++) {
+        social[i].style.visibility = "visible";
+    }
 }
