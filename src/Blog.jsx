e3cb4599792b653c5a63ab102eb6fc03778253f9
pravi-97 <praveesh.894@gmail.com>
2023-10-19 14:12:52 +0530

    add blog specific codes

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,7 +1,42 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./styles/Blog.css";
 const Blog = () => {
-    return ( 
-        <h1>Blog</h1>
-     );
-}
- 
+  const { id } = useParams();
+  // console.log("id: ", id);
+    const [blog, setBlog] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+  let bodyParts = [];
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/get/"+id)
+      .then((response) => {
+        setBlog(response.data.message[0]);
+        setLoading(false);
+        bodyParts = (blog.body);
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+  
+
+    if (loading) {
+      return (
+        <div className="d-flex justify-content-center">
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      );
+    }
+  return (
+    <hello></hello>
+  );
+};
+
 export default Blog;
---
+++ src/Blogs.jsx
@@ -0,0 +1,63 @@
+import "./styles/Blogs.css";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const createdAt = new Date();
+createdAt.setDate(createdAt.getDate());
+const Blogs = () => {
+  const [blogList, setBlogList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://yavvlidt1i.execute-api.ap-south-1.amazonaws.com/prod/")
+      .then((response) => {
+        setBlogList(response.data.message);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="d-flex justify-content-center">
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+  return (
+    <section>
+      <h1>Blogs</h1>
+      <div className="container">
+        {blogList.map((blog, index) => (
+          <Link className="navbar-brand" to={`/blog/${blog.title}`}>
+            <div className="row" key={index}>
+              <div className="col-md-12">
+                <div className="blog-content">
+                  <div className="text-area">
+                    <h2>{blog.title}</h2>
+                    <p>{blog.smallHeading}</p>
+                    <div className="created-at">{Date(blog.created_at)}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Blogs;
