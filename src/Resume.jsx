1c192dc4a3f46043353701a9317e7f36e79464f1
pravi-97 <praveesh.894@gmail.com>
2024-02-05 13:44:35 +0530

    add Resume changes

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,11 +1,25 @@
 import React, { useEffect, useState } from "react";
 import Loader from "./Loader";
+import "./styles/Resume.css";
 
 const Resume = () => {
   useEffect(() => {
     document.title = "Praveesh P | Resume";
   }, []);
-  return <Loader/>;
+
+  return (
+    <>
+      <iframe
+        src="https://docs.google.com/gview?url=https://praveesh-resume.s3.ap-south-1.amazonaws.com/Praveesh_Resume.docx&embedded=true"
+        style={{
+          width: "100%",
+          height: "100vh",
+          border: "none",
+          backgroundColor: "white",
+        }}
+      ></iframe>
+    </>
+  );
 };
 
 export default Resume;
