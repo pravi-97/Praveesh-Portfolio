92508b4a0e5a83bf47faec5da1c766c940d91b69
pravi-97 <129660831+pravi-97@users.noreply.github.com>
2024-10-22 14:53:28 +0530

    add about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,33 @@
-import './styles/About.css'
+import "./styles/About.css";
 const About = () => {
   return (
-    <section>
-      <div id="about-about-section">
-        About
+    <section id="about-about-section">
+      <div className="container">
+        <div className="row">
+          <h1>About Me</h1>
+          <div className="col-md-12">
+            <p>
+              &nbsp;Hi, I'm Praveesh, a software engineer passionate about crafting
+              efficient and scalable solutions. With expertise in Java
+              development, cloud solutions, and full-stack web development, I
+              thrive on bringing ideas to life through code.
+            </p>
+            <p>
+              I have over three years of experience as a Systems
+              Engineer at Infosys, where I built solutions for clients based on
+              their business requirements. My primary focus is backend Java
+              development, and I have a strong grasp of React.js, having
+              developed multiple full-stack projects.
+            </p>
+            <p>
+              Currently, I work as a Java Backend Developer at Incture
+              Technologies, where I focus on writing efficient and scalable code
+              to deliver robust solutions. I strive to implement best practices
+              and innovative approaches that contribute to the overall success
+              of our projects.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
